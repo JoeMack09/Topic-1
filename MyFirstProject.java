7226e4204c292ecc92e892e17c188f3548dd845b
JoeMack09 <68520433+JoeMack09@users.noreply.github.com>
2020-08-16 21:33:47 -0700

    Add files via upload

---
+++ MyFirstProject.java
@@ -0,0 +1,9 @@
+public class MyFirstProject {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Anthony McCormack.");
+		System.out.println("My favorite color is red");
+		System.out.println("If I won the lottery, I would pay off my debt and invest the rest of the money.");
+	}
+
+}
